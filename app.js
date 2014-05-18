f451566c8d0cc370da3ab3f7e70eefe9bf555d76
Trevor <trevorjbaron@gmail.com>
2014-05-18 14:01:51 -0400

    bitpay api

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ var https = require('https')
 var http = require('http')
 var request = require('request');
 var fs = require('fs')
+var cors = require('koa-cors');
+var thunkify = require('thunkify');
 var app = koa()
+var Promise = require("bluebird");
+var BitPay = require('bitpay-node');
+var client = new BitPay.Client({ apiKey: 'BUxuipHFna0BXrzCwl7l6LWX2eURfio2MsKFq8'});
 
 //REMOVE IN PRODUCTION??
 swig.setDefaults(config.templateOptions)
@@ -23,11 +28,13 @@ swig.setDefaults(config.templateOptions)
 app.keys = [config.sessionSecret]
 app.use(session())
 app.use(jsonResp())
+app.use(cors())
 app.use(router(app))
 
 //PAGE ROUTES
 app.get('/', defaultPageLoad('index'))
 app.get('/order', defaultPageLoad('order'))
+app.post('/invoice', invoice)
 app.get('/public/*', serve('.'))
 
 
@@ -42,6 +49,28 @@ function defaultPageLoad(pageName, requiresLogin) {
 		var temp = sessionHelper.commonTemplate(this.session);
 		this.body = yield render(pageName, temp)
 	}
+}
+
+function *invoice (){
+	var amount = parseFloat(this.query.p)
+	var that = this
+	var ret = yield bitpayer(amount)
+	this.jsonResp(200, ret)
+}
+
+function bitpayer(amount){
+	var resolver = Promise.pending();
+
+	var invoiceOptions = { 
+	  price: amount, 
+	  currency: 'USD' 
+	}; 
+
+	client.createInvoice(invoiceOptions, function(err, invoice) {
+	  resolver.resolve(invoice);
+	})
+
+	return resolver.promise;
 }
 
 
