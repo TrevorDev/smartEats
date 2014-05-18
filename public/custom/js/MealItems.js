8ff3d34d36a79f9c0dad4cbe08f8f77be965df98
Shabbir Hussain <mohd.husn001@gmail.com>
2014-05-17 20:02:41 -0400

    populated mealItems.js with titles and images

--- public/custom/js/MealItems.js
+++ public/custom/js/MealItems.js
@@ -15,7 +15,12 @@ function populateMealList(){
 	var Meals=[];
 	
 	
-	var Tacos = new mealItem(1,"Lime Chicken Soft Tacos", [ new ingredientItem("",), ], "");
+	var Tacos = new mealItem(1,"Lime Chicken Soft Tacos", [ new ingredientItem("",), ], "../public/custom/img/taco.jpg");
+	var Tacos = new mealItem(2,"Asparagus, Chicken, and Pecan Pasta", [ new ingredientItem("",), ], "../public/custom/img/pasta.jpg");
+	var Tacos = new mealItem(3,"Grilled Shrimp Caprese", [ new ingredientItem("",), ], "../public/custom/img/shrimp.jpg");
+	var Tacos = new mealItem(4,"Tomato Cream Sausce for Pasta", [ new ingredientItem("",), ], "../public/custom/img/tomatocream.jpg");
+	var Tacos = new mealItem(5,"Easter breakfast casserole", [ new ingredientItem("",), ], "../public/custom/img/easter.jpg");
+	var Tacos = new mealItem(6,"Strawberry Bread", [ new ingredientItem("",), ], "../public/custom/img/bread.jpg");
 	
 	Meals.push(Rosee);
 	
