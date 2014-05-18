da336529b7d60c2993e400c488ad30260aad8697
Shabbir Hussain <mohd.husn001@gmail.com>
2014-05-17 20:44:17 -0400

    added code to add meal to list

--- public/custom/js/MealAutocomplete.js
+++ public/custom/js/MealAutocomplete.js
@@ -2,18 +2,38 @@ $(function(){
 	
 	console.log( "ready!" );
 	//=================declare page elements========================
-	
+	var meals = populateMealList();	
+	var userChosenMeals=[1,2,3,5];
 	
 	//=============prototype functions==============================
 	//handle adding a meal
-	$("#addMealButton").click(function(mealItem){
-    console.log( "Meal Added!" );
+	$("#addMeal_button").click(function(mealItem){
+		
+		//get text input
+		var mealInputString = $("#s").val();
+		
+		var found =0;
+		for(var i=0; i<meals.length;i++){
+			if(mealInputString === meals[i].Title){
+				userChosenMeals.push(i);
+				found =1;
+				break;
+			}
+		}
+		
+		//handle the case of invalid input
+		if(found===0){
+		
+		}
+		
+	
+		console.log( "Meal Added!" );
   });
 
   $("#checkout").click(function(){
-    var testIds = [1,2,3,5]
-    window.location="/order?items="+encodeURIComponent(testIds)
-    return false
-  })
+    
+    window.location="/order?items="+encodeURIComponent(userChosenMeals);
+    return false;
+  });
  
 });
